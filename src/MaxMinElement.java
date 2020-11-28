a2417893a342c451a4f68ccb8fa798cebfc6f683
r0e009w <ravichandra.enaganti@walmart.com>
2020-11-29 00:16:54 +0530

    Exception cases for maxmin element

--- src/MaxMinElement.java
+++ src/MaxMinElement.java
@@ -4,6 +4,8 @@ public class MaxMinElement {
     public static void main(String []args) {
         int []arr = {9, 69, 42, 36, 60};
         System.out.println(maxInteger(arr));
+        System.out.println(maxInteger(null)); //Throws Null pointer exception
+        System.out.println(maxInteger(new int[]{})); //Throws NoSuchElement exception
     }
 
     private static int maxInteger(int []arr) {
